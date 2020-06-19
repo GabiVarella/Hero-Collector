14e9cef68b645f53eb482f2c4fdf4d5524a71eae
Gabriel Varella <gabrielvarella@Gabriels-MacBook-Pro.local>
2020-06-18 20:51:37 -0700

    last commit of the day

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Hero, Enemy
 from .forms import WeaponForm
 
 
 # Create your views here.
+
+class HeroCreate(CreateView):
+  model = Hero
+  fields = '__all__'
+  success_url = '/heroes/'
+
+
+class HeroUpdate(UpdateView):
+  model = Hero
+  fields = ['alter_ego', 'super_powers', 'arch_enemy']
+
+class HeroDelete(DeleteView):
+  model = Hero
+  success_url = '/heroes/'
 
 def home(request):
   return render(request, 'home.html')
@@ -21,7 +36,7 @@ def heroes_detail(request, hero_id):
   hero = Hero.objects.get(id=hero_id)
   enemies_hero_doesnt_have = Enemy.objects.exclude(id__in = hero.enemies.all().values_list('id'))
   weapon_form = WeaponForm()
-  return render(request, 'heroes/detail.html', {'hero': hero, 'weapon_form': weapon_form})
+  return render(request, 'heroes/detail.html', {'hero': hero, 'weapon_form': weapon_form, 'enemies': enemies_hero_doesnt_have})
 
 def assoc_enemy(request, hero_id, enemy_id):
   # Note that you can pass a toy's id instead of the whole object
@@ -41,16 +56,7 @@ def add_weapon(request, hero_id):
     new_weapon.save()
   return redirect('detail', hero_id=hero_id)
 
-class HeroCreate(CreateView):
-  model = Hero
-  fields = '__all__'
-  success_url = '/heroes/'
 
 
-class HeroUpdate(UpdateView):
-  model = Hero
-  fields = ['alter_ego', 'super_powers', 'arch_enemy']
-
-class HeroDelete(DeleteView):
-  model = Hero
-  success_url = '/heroes'
+class EnemyList(ListView):
+  model = Enemy
